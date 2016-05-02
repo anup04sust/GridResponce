1fb6284a017284d717c3b418f177c855551b3624
anup <anup@ergo-ventures.com>
2016-05-02 18:19:55 +0600

    initialize

---
+++ config.rb
@@ -0,0 +1,12 @@
+require 'compass/import-once/activate'
+#http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from server root.
+relative_assets = true
